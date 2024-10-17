b8b66f66814345fa77ae83feb206b8d8ec2683ac
ashik3132 <imashikur1@gmail.com>
2024-10-18 00:10:14 +0600

    first commit

---
+++ for_loop.php
@@ -0,0 +1,45 @@
+<?php
+
+
+for ($i = 2; $i<=10; $i+=2){
+    // if($i % 2 == 0){
+    //     echo $i . "\n"
+    // }
+    echo " this is $i" . "\n";
+}
+
+
+
+// for ($i = 2; $i<=12; $i+=2){
+//     if($i % 3 == 1){
+//         echo $i . "\n";
+//     }
+//     // echo $i . "\n";
+// }
+
+
+// echo"This is heading\n";
+// $starCount=100;
+// for ($i = 1; $i <= $starCount; $i++) {
+//     echo "*";
+// }
+// echo "\n";
+// echo"this is parragraph this is parragraph this is parragraph this is parragraph\n";
+// for ($i = 1; $i <= $starCount ; $i++) {
+//     echo "*";
+// }
+// echo "\n";
+// echo "this is sign";
+
+
+
+// echo"This is heading\n";
+// $starCount=100;
+// for ($i = 1; $i <= $starCount; $i++) {
+//     if ($i %5 == 1) {
+//         echo "$i" ."\n";
+//     }
+
+// }
+
+
